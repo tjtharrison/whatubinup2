39ea64c5a1b64b18fed3d830c52afea9ba688af5
tjtharrison <tjtharrison@users.noreply.github.com>
2022-07-07 19:10:56 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 """
 Script to install whatubinup2
 """
-from setuptools import setup
 from glob import glob
 
+from setuptools import setup
+
 if __name__ == "__main__":
-    setup(
-        data_files = [
-        ('whatubinup2', glob('*', recursive=True))
-        ]
-    )
+    setup(data_files=[("whatubinup2", glob("*", recursive=True))])
