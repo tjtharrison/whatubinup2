4232beacf1fbf104682e7ab969cfe493d9beb91c
tjtharrison <tjtharrison@users.noreply.github.com>
2022-07-07 19:29:14 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -6,7 +6,4 @@ from glob import glob
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(data_files=[
-            ("whatubinup2", set(glob("*")) - set(glob("*.egg-info")))
-        ]
-    )
+    setup(data_files=[("whatubinup2", set(glob("*")) - set(glob("*.egg-info")))])
