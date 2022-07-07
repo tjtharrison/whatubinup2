9faaf724c6f05d817fe67ae45409f6af8bb79d7b
tjtharrison <tjtharrison@users.noreply.github.com>
2022-07-07 20:14:57 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
         url="https://github.com/teamjtharrison/what_u_bin_up_2",
         long_description=pypandoc.convert_file("README.md", "rst"),
         long_description_content_type="text/markdown",
-        package_dir = {'': 'whatubinup2'},
-        packages= ['bin',]
+        package_dir={"": "whatubinup2"},
+        packages=[
+            "bin",
+        ],
     )
