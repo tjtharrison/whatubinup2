140ec8dbf234b849c7deebf13e89304c883f3a6f
tjtharrison <gh@tjth.co>
2022-07-07 20:29:01 +0100

    PR/Fixed egg file missing from setup [patch]

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ from glob import glob
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(data_files=[("whatubinup2", glob("*", recursive=True))])
+    setup(data_files=[
+            ("whatubinup2", set(glob("*")) - set(glob("*.egg-info")))
+        ]
+    )
