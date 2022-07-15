6b941e3568c4769cc0480f757b72f87196980e97
tjtharrison <tjtharrison@users.noreply.github.com>
2022-07-15 20:43:57 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ Used by py2app to create .app
 
 from setuptools import setup
 
-APP = [
-    'src/whatubinup2/__main__.py'
-]
+APP = ["src/whatubinup2/__main__.py"]
 DATA_FILES = []
 OPTIONS = {
-    'iconfile':'img/wubu2.icns',
+    "iconfile": "img/wubu2.icns",
 }
 
 setup(
     app=APP,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
