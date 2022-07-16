d6cdfdfe2541a86e7ed37ef28bf8accf54fe3f2d
tjtharrison <gh@tjth.co>
2022-07-16 22:39:16 +0100

    WIP/Separate setup for win

--- setup-win.py
+++ setup-win.py
@@ -14,6 +14,6 @@ OPTIONS = {
 setup(
     app=APP,
     data_files=DATA_FILES,
-    options={"py2app": OPTIONS},
-    setup_requires=["py2app"],
+    options={"py2exe": OPTIONS},
+    setup_requires=["py2exe"],
 )
