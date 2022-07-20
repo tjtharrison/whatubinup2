33c436796ca8ae7fe344438e85f0690589b4e4e5
tjtharrison <tjtharrison@users.noreply.github.com>
2022-07-20 18:25:11 +0000

    Linted and formatted files

--- buttons.py
+++ buttons.py
@@ -1,4 +1,3 @@
-
 import sys
 
 if sys.version_info[0] >= 3:
@@ -8,29 +7,29 @@ else:
 
 buttons_col = []
 for i in range(5):
-    buttons_col.append([sg.Button('col {}'.format(i))])
+    buttons_col.append([sg.Button("col {}".format(i))])
 
 buttons_row = []
 for i in range(5):
-    buttons_row.append(sg.Button('row {}'.format(i)))
+    buttons_row.append(sg.Button("row {}".format(i)))
 
 layout = [
-            [sg.Text('Your typed chars appear here:'), sg.Text('', key='_OUTPUT_')],
-            *buttons_col,
-            [*buttons_row],
-            [sg.Input(do_not_clear=True, key='_IN_')],
-            [sg.Button('Show'), sg.Button('Exit')]
-         ]
+    [sg.Text("Your typed chars appear here:"), sg.Text("", key="_OUTPUT_")],
+    *buttons_col,
+    [*buttons_row],
+    [sg.Input(do_not_clear=True, key="_IN_")],
+    [sg.Button("Show"), sg.Button("Exit")],
+]
 
-window = sg.Window('Window Title').Layout(layout)
+window = sg.Window("Window Title").Layout(layout)
 
-while True:             # Event Loop
+while True:  # Event Loop
     event, values = window.Read()
     print(event, values)
-    if event is None or event == 'Exit':
+    if event is None or event == "Exit":
         break
-    if event == 'Show':
+    if event == "Show":
         # change the "output" element to be the value of "input" element
-        window.FindElement('_OUTPUT_').Update(values['_IN_'])
+        window.FindElement("_OUTPUT_").Update(values["_IN_"])
 
 window.Close()
